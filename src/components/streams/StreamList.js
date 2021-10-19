fe6e59568938ffed2152c9070d778ec46af7a7ab
kokoaono <nyswzt.a@gmail.com>
2021-10-20 12:26:37 +1300

    bacis app frame

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -12,8 +12,12 @@ class StreamList extends React.Component {
     if(stream.userId === this.props.currentUserId){
       return(
         <div className="right floated content">
-          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
-          <Link to={`/streams/delete/${stream.id}`} className="ui button negative">Delete</Link>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
+          <Link to={`/streams/delete/${stream.id}`} className="ui button negative">
+            Delete
+          </Link>
         </div>
       )
     }
@@ -26,7 +30,9 @@ class StreamList extends React.Component {
            {this.renderAdmin(stream)}
           <i className='large middle aligned icon camera' />
           <div className='content'>
+            <Link to={`/streams/${stream.id}`} className="header">
             {stream.title}
+            </Link>
             <div className='description'>{stream.description}</div>
           </div>
          
